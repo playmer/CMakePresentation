14e51cf952fe2a659649ee1b6fcebab1d6c04a0a
Joshua Shlemmer <joshuashlem@gmail.com>
2018-07-10 20:31:56 -0700

    Started to do external lib stuff.

---
+++ Examples/02. ExternalLibs/src/myLib/myLib.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int myLibsVeryImportantFunction()
+{
+    // we C++ we gotta use the features!
+    try
+    {
+        std::cout << "Woo this important function!";
+
+        // now lets zip ourselves, that sounds pretty fun!
+        
+
+        return 0;
+    }
+    catch(...)
+    {
+        return 1;
+    }
+}
