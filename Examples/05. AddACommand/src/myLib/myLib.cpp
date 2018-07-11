5558c310cee0d81a39d792979c3c0aeb1ba3665c
Joshua Shlemmer <joshuashlem@gmail.com>
2018-07-11 12:40:20 -0700

    Added 5th example.

---
+++ Examples/05. AddACommand/src/myLib/myLib.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+#include <GLFW/glfw3.h>
+
+int myLibsVeryImportantFunction()
+{
+    // we C++ we gotta use the features!
+    try
+    {
+        std::cout << "Woo this important function!";
+
+        // now lets make a window, sounds like fun!
+        
+        GLFWwindow* window;
+
+        /* Initialize the library */
+        if (!glfwInit())
+            return -1;
+
+        /* Create a windowed mode window and its OpenGL context */
+        window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+        if (!window)
+        {
+            glfwTerminate();
+            return -1;
+        }
+
+        /* Make the window's context current */
+        glfwMakeContextCurrent(window);
+
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(window))
+        {
+            /* Swap front and back buffers */
+            glfwSwapBuffers(window);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
+
+        glfwTerminate();
+
+        return 0;
+    }
+    catch(...)
+    {
+        return 1;
+    }
+}
